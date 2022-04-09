6b9208ee9e4f99719f9dd8a25ce95275b6a56c5e
Sevil Aydın <sevar2362@gmail.com>
2022-04-09 23:03:16 +0300

    Add files via upload

---
+++ WFA_KimBilir(Kim Yapar)/WFA_KimBilir/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace WFA_KimBilir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnBilirKisi = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(241, 452);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnBilirKisi
+            // 
+            this.btnBilirKisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBilirKisi.Location = new System.Drawing.Point(538, 392);
+            this.btnBilirKisi.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBilirKisi.Name = "btnBilirKisi";
+            this.btnBilirKisi.Size = new System.Drawing.Size(273, 55);
+            this.btnBilirKisi.TabIndex = 2;
+            this.btnBilirKisi.Text = "Konu Belirle";
+            this.btnBilirKisi.UseVisualStyleBackColor = true;
+            this.btnBilirKisi.Click += new System.EventHandler(this.btnBilirKisi_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(262, 15);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(241, 452);
+            this.listBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1559, 545);
+            this.Controls.Add(this.btnBilirKisi);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnBilirKisi;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ WFA_KimBilir(Kim Yapar)/WFA_KimBilir/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_KimBilir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] ogrenciAdlari = { "SEVİL","EDEBALİ MERT", "AZAD", "SILA", "UMUT", "SARUHAN MERT", "ONUR", "Emre", "ENES", "FURKAN SEMİH", "KAAN", "BAHADIR", "MERT", "BURAK", "AHMET CANER", "TUBA" };
+        string[] ogrenciSoyadlari = {"AYDIN", "ATALAY", "MAJIDLI", "ŞİRİN", "YILBAŞ", "DEMİRAY", "DOĞRU", "BEKTAŞOĞLU", "SERENLİ", "GÜNEŞ", "ÜNAL", "ARDA", "ÇINAR", "UYSAL", "TAI", "KARADUT BUĞDAY" };
+        string[] konular = { "if-else", "While", "Do-While", "Foreach", "Hata Yonetimi", "Constructor", "İnterface", "Polimorphism", "Abstaraction", "Encapsilacion", "İnheritance", "Static", };
+
+        Random rnd = new Random();
+        Random rnd2 = new Random();
+        Random rnd3=new Random();
+        int konu;
+        int ad;
+        int soyad;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ogrenciAdlari.Length && i<ogrenciSoyadlari.Length; i++)
+            {
+                
+                listBox1.Items.Add(ogrenciAdlari[i] + ogrenciSoyadlari[i]);
+            }
+
+            for (int j = 0; j < konular.Length; j++)
+            {
+                listBox2.Items.Add(konular[j]);
+            }
+            
+        }
+
+        private void btnBilirKisi_Click(object sender, EventArgs e)
+        {
+            ad=rnd.Next(0,ogrenciAdlari.Length);
+            soyad=rnd2.Next(0,ogrenciSoyadlari.Length);
+            konu=rnd3.Next(0,konular.Length);
+
+            MessageBox.Show("Kişi"+" "+ogrenciAdlari[ad]+" "+ogrenciSoyadlari[soyad]+" "+"Konu"+" "+konular[konu]);
+
+            listBox1.Items.Remove(ogrenciAdlari[ad]+ogrenciSoyadlari[soyad]);
+
+
+
+        }
+    }
+}
